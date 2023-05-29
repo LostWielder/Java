96d579fb4cec10f766860c0eda3f73bb96c2a91b
Jaron Swartz <NanarieRyoka@gmail.com>
2023-05-29 07:41:32 -0400

    MiniGame Project
    Minigame compilation from school

--- MiniGameLauncher/src/main/java/com/example/minigamelauncher/checkersfx/MoveType.java
+++
@@ -1,7 +0,0 @@
-package com.example.minigamelauncher.checkersfx;
-/**
- * @author Almas Baimagambetov (almaslvl@gmail.com)
- */
-public enum MoveType {
-    NONE, NORMAL, KILL
-}
--- MiniGameLauncher/src/main/java/com/example/minigamelauncher/ttt/Main.java
+++
@@ -1,7 +0,0 @@
-package com.example.minigamelauncher.ttt;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.main(args);
-    }
-}
